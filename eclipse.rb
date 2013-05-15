8fdd087b0cae849776c0c2c1d400386ab8fc3766
Erran Carey <me@errancarey.com>
2013-05-15 16:23:38 -0500

    Update Eclipse formula

--- eclipse.rb
+++ eclipse.rb
@@ -7,8 +7,13 @@ class Eclipse < Formula
   url 'http://ftp.osuosl.org/pub/eclipse/technology/epp/downloads/release/juno/SR2/eclipse-jee-juno-SR2-macosx-cocoa-x86_64.tar.gz'
   sha1 '40d61b8627e902cada4d4d1b742c600b5d2f450d'
 
+  # Eclipse => About Eclipse
+  # Juno Service Release 2 - Build 20130225-0426
+  # 1.5.2.201302211-1820
+  version '1.5.2'
+
   def install
-    bin.install 'eclipse'
+    prefix.install Dir['*']
   end
 
   test do
